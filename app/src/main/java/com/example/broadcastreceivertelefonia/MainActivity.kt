9a312096c514b2dc78fa39262a62dd360290fcb1
Clementain <clentecarmona@gmail.com>
2023-03-11 20:11:06 -0600

    Ya se envian mensajes cuando llega una llamada

--- app/src/main/java/com/example/broadcastreceivertelefonia/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceivertelefonia/MainActivity.kt
@@ -6,18 +6,29 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var txtPhoneNumber: TextView
+    private lateinit var txtTargetNumber: TextView
+    private lateinit var txtMsg: TextView
+
 
     private val incomingCallReceiver = object : BroadcastReceiver() {
         @SuppressLint("SetTextI18n")
         override fun onReceive(context: Context?, intent: Intent?) {
             val incomingNumber = intent?.getStringExtra("incoming_number")
             txtPhoneNumber.text = "Llamada entrante de $incomingNumber"
+            if (incomingNumber == txtTargetNumber.text.toString()) {
+                val smsManager = SmsManager.getDefault()
+                smsManager.sendTextMessage(
+                    txtTargetNumber.text.toString(), null, txtMsg.text.toString(), null, null
+                )
+            }
+
         }
     }
 
@@ -26,6 +37,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         txtPhoneNumber = findViewById(R.id.Telefono)
+        txtTargetNumber = findViewById(R.id.txtTelefono)
+        txtMsg = findViewById(R.id.txtMensaje)
 
         val filter = IntentFilter("com.example.myapp.NEW_INCOMING_CALL")
         registerReceiver(incomingCallReceiver, filter)
